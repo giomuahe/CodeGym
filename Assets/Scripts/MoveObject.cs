f88abcf5d84af6cca0695565c1952b971658d214
cavoisatthu <thanhbd92@gmail.com>
2024-10-03 17:15:48 +0700

    leason 1 + 2 first commit

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mecibucu");
+        Debug.LogWarning("ZDa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.tra
+    }
+}
